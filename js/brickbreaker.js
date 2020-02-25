fb7097517794bc85ab49d459a2107d5de35d4277
Gabrielle Weiner <gweiner@ucvts.org>
2020-02-25 12:49:03 -0500

    got it to work again:

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -29,6 +29,7 @@ for(var c=0; c<brickColumnCount; c++) {
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
@@ -46,6 +47,13 @@ function keyUpHandler(e) {
     else if(e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = false;
     }
+}
+
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if(relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth/2;
+  }
 }
 function collisionDetection() {
   for(var c=0; c<brickColumnCount; c++) {
