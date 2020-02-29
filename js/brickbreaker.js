0463ae26518f91fec867c25237a162ea42675c30
EMILY STEINER <emilysteiner@EMILYs-iMac.local>
2020-02-29 15:33:12 -0500

    added function

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -39,7 +39,14 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
-
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
 
 function keyUpHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
