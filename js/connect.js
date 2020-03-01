1c7c06f3ed90b1d67a29a860224027c883e3719e
EMILY STEINER <emilysteiner@EMILYs-iMac.local>
2020-02-29 19:14:23 -0500

    deleted the irrelevant comments I wrote and added two functions

--- js/connect.js
+++ js/connect.js
@@ -64,7 +64,6 @@ class Cell {
       // color
       color = this.winner ? COLOR_WIN : this.highlight ? COLOR_PLAY : COLOR_COMP;
 
-      // draw a circle around the perimeter
       ctx.lineWidth = this.r / 4;
       ctx.strokeStyle = color;
       ctx.beginPath();
@@ -102,7 +101,7 @@ function loop(timeNow) {
   }
 
   // calculate the time difference
-  timeDelta = (timeNow - timeLast) / 1000; // seconds
+  timeDelta = (timeNow - timeLast) / 1000; // sec
   timeLast = timeNow;
 
   // update
@@ -146,7 +145,6 @@ function checkWin(row, col) {
       }
   }
 
-  // if any have four in a row, return a win!
   return connect4(diagL) || connect4(diagR) || connect4(horiz) || connect4(vert);
 }
 
@@ -155,29 +153,24 @@ function connect4(cells = []) {
   let winningCells = [];
   for (let i = 0; i < cells.length; i++) {
 
-      // no owner, reset the count
       if (cells[i].owner == null) {
           count = 0;
           winningCells = [];
       }
 
-      // same owner, add to the count
       else if (cells[i].owner == lastOwner) {
           count++;
           winningCells.push(cells[i]);
       }
 
-      // new owner, new count
       else {
           count = 1;
           winningCells = [];
           winningCells.push(cells[i]);
       }
 
-      // set the lastOwner
       lastOwner = cells[i].owner;
 
-      // four in a row is a win
       if (count == 4) {
           for (let cell of winningCells) {
               cell.winner = true;
@@ -205,24 +198,20 @@ function click(ev) {
 function createGrid() {
   grid = [];
 
-  // set up cell size and margins
   let cell, marginX, marginY;
 
-  // portrait
   if ((width - margin * 2) * GRID_ROWS / GRID_COLS < height - margin * 2) {
       cell = (width - margin * 2) / GRID_COLS;
       marginX = margin;
       marginY = (height - cell * GRID_ROWS) / 2;
   }
 
-  // landscape
   else {
       cell = (height - margin * 2) / GRID_ROWS;
       marginX = (width - cell * GRID_COLS) / 2;
       marginY = margin;
   }
 
-  // populate the grid
   for (let i = 0; i < GRID_ROWS; i++) {
       grid[i] = [];
       for (let j = 0; j < GRID_COLS; j++) {
@@ -240,7 +229,6 @@ function drawBackground() {
 
 function drawGrid() {
 
-  // frame and butt
   let cell = grid[0][0];
   let fh = cell.h * GRID_ROWS;
   let fw = cell.w * GRID_COLS;
@@ -262,7 +250,6 @@ function drawText() {
       return;
   }
 
-  // set up text parameters
   let size = grid[0][0].h;
   ctx.fillStyle = gameTied ? COLOR_TIE : playersTurn ? COLOR_PLAY : COLOR_COMP;
   ctx.font = size + "arial";
@@ -272,7 +259,6 @@ function drawText() {
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
 
-  // draw the text
   let offset = size * 0.55;
   let text = gameTied ? TEXT_TIE : playersTurn ? TEXT_PLAY : TEXT_COMP;
   if (gameTied) {
@@ -291,7 +277,6 @@ function goComputer(delta) {
       return;
   }
 
-  // count down till the computer makes its selection
   if (timeComp > 0) {
       timeComp -= delta;
       if (timeComp <= 0) {
@@ -300,51 +285,42 @@ function goComputer(delta) {
       return;
   }
 
-  // set up the options array
   let options = [];
   options[0] = []; // computer wins
   options[1] = []; // block the player from winning
-  options[2] = []; // not important
+  options[2] = []; // not priority
   options[3] = []; // give away a win
 
-  // loop through each column
   let cell;
   for (let i = 0; i < GRID_COLS; i++) {
       cell = highlightCell(grid[0][i].cx, grid[0][i].cy);
 
-      // column full, go to the next column
       if (cell == null) {
           continue;
       }
 
-      // first priority, computer wins
       cell.owner = playersTurn;
       if (checkWin(cell.row, cell.col)) {
           options[0].push(i);
       } else {
 
-          // second priority, block the player
           cell.owner = !playersTurn;
           if (checkWin(cell.row, cell.col)) {
               options[1].push(i);
           } else {
               cell.owner = playersTurn;
 
-          // check the cell above
             if (cell.row > 0) {
                 grid[cell.row - 1][cell.col].owner = !playersTurn;
 
-                // last priority, let player win
                 if (checkWin(cell.row - 1, cell.col)) {
                     options[3].push(i);
                 }
 
-                // third priority, no significance
                 else {
                     options[2].push(i);
                 }
 
-                // deselect cell above
                 grid[cell.row - 1][cell.col].owner = null;
             } else {
                   options[2].push(i);
@@ -352,19 +328,16 @@ function goComputer(delta) {
           }
       }
 
-      // cancel highlight and selection
       cell.highlight = null;
       cell.owner = null;
   }
 
-  // clear the winning cells
   for (let row of grid) {
       for (let cell of row) {
           cell.winner = false;
       }
   }
 
-  // randomly select a column in priority order
   let col;
   if (options[0].length > 0) {
       col = options[0][Math.floor(Math.random() * options[0].length)];
@@ -376,10 +349,10 @@ function goComputer(delta) {
       col = options[3][Math.floor(Math.random() * options[3].length)];
   }
 
-  // highlight the selected cell
+
   highlightCell(grid[0][col].cx, grid[0][col].cy);
 
-  // set the delay
+
   timeComp = DELAY_COMP;
 }
 
@@ -387,11 +360,8 @@ function highlightCell(x, y) {
   let col = null;
   for (let row of grid) {
       for (let cell of row) {
-
-          // clear existing highlighting
           cell.highlight = null;
 
-          // get the column
           if (cell.contains(x, y)) {
               col = cell.col;
           }
@@ -402,7 +372,6 @@ function highlightCell(x, y) {
     return;
 }
 
-// highlight the first unoccupied cell
 for (let i = GRID_ROWS - 1; i >= 0; i--) {
     if (grid[i][col].owner == null) {
         grid[i][col].highlight = playersTurn;
@@ -410,4 +379,18 @@ for (let i = GRID_ROWS - 1; i >= 0; i--) {
     }
 }
 return null;
+}
+
+function highlightGrid(ev) {
+  if (!playersTurn || gameOver) {
+      return;
+  }
+  highlightCell(ev.clientX, ev.clientY);
+}
+
+function newGame() {
+  playersTurn = Math.random() < 0.5;
+  gameOver = false;
+  gameTied = false;
+  createGrid();
 }
